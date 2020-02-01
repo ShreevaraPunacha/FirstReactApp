7e38ef47c77ff491e048af52ca2a8e6ca79dfe6b
Shreevara Punacha <shrivarabhat123@gmail.com>
2020-02-01 23:00:43 +0530

    First Commit with State and Set State Function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class Car extends React.Component {
+
+  constructor(props){
+      super(props);
+        this.state = {
+            brand: "Ford",
+            model:"Mustang",
+            color:"red",
+            year: 1964
+        };
+  }
+  changeColor = () =>{
+      this.setState({color:"blue"});
+  }
+
+  render(){
+      return (
+          <div>
+              <h1>
+                  My {this.state.brand}
+              </h1>
+              <p>
+                  It is a {this.state.color}
+                  {this.state.model}
+                  from {this.state.year}
+              </p>
+              <button
+              type="button"
+              onClick = {this.changeColor}>
+                  Change color
+              </button>
+          </div>
+      )
+  }
+}
+
+
+
+
+ReactDOM.render(<Car/>, document.getElementById('root'));
+

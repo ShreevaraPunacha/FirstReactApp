0ebdd21fe4f3743c206948f166a261891b90c6fa
Shreevara Punacha <shrivarabhat123@gmail.com>
2020-02-02 11:47:17 +0530

    My First React Form

--- src/index.js
+++ src/index.js
@@ -108,8 +108,22 @@ class Child extends React.Component {
 }
 
 
+class MyForm extends React.Component {
+    render(){
+        return (
+            <form>
+                <h1>Hello</h1>
+                <p>Enter your name:</p>
+                <input
+                    type="text"
+                />
+            </form>
+        )
+    }
+    
+}
 
 
 
-ReactDOM.render(<Container />, document.getElementById('root'));
+ReactDOM.render(<MyForm />, document.getElementById('root'));
 
